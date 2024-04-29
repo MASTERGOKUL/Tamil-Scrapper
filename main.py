4e62faf7631baff542203f43207885bf1ddd3fbc
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2024-04-29 09:44:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import re
+from urllib.error import HTTPError
 import pandas
 import requests
 from bs4 import BeautifulSoup
+
 
 def filter_non_tamil_text(texts: list) -> list[list]:
     """
@@ -41,7 +43,7 @@ def filter_tamil_text(texts: list) -> list[list]:
     return tamil_text
 
 
-def get_web_content(link) -> BeautifulSoup:
+def get_web_content(link: str) -> BeautifulSoup:
     """
     To get the bs4 version of the html page.
 
@@ -53,34 +55,60 @@ def get_web_content(link) -> BeautifulSoup:
     return web_content
 
 
-class TamilScarper:
+class TamilScraper:
     def __init__(self, link: str):
         """
-        Tamil Scraper is a tool to scarp tamil contents from online.
+        Tamil Scraper is designed to extract Tamil text from online sources.
+
+        It utilizes web scraping techniques to retrieve Tamil content from the specified URL.
+
+        Example:
+
+        >>> import TamilScraper
+        >>> tamil_scrap = TamilScraper('https://www.projectmadurai.org/pm_etexts/utf8/pmuni0002.html')
+        >>> tamil_scrap.get_text() # gives the text of tamil characters inside any tag (may contain non-tamil characters).
+        >>> tamil_scrap.get_by_tag('h1') # gives the text of tamil characters inside h1 tag (may contain non-tamil characters).
+        >>> tamil_scrap.get_table() # gives the tables that contains any tamil character.
+
         :param link: URL of the website to be scrapped
         """
         self.link = link
         self.web_content = get_web_content(link)
 
-    def get_text(self, only_other: bool = False, only_tamil: bool = False) -> list | list[list]:
+    def get_text(self, only_other: bool = False, only_tamil: bool = False) -> list[str] | list[list]:
         """
         Gives the tamil text(may contain non-tamil characters) (or) only tamil text (or) only non-tamil text, based on the
         parameters in the hierarchical order of the webpage.
 
+        ---
+        NOTE
+        ---
+
+        if this function returns empty array it means there is no content available
+
+        (or)
+
+        The Website you are looking is NOT ALLOWING content to be scraped
+
+
         :param only_other: True, if only non-tamil characters needed
         :param only_tamil: True, if only tamil characters needed
-        :return:
+        :return: list[str]: if the function is called normally; list[list]: only_tamil (or) only_other is True.
+        :raise ValueError: If both only_other and only_tamil are True, for one function call one type of output
         """
 
-        # to get text except tamil characters
+        if only_tamil and only_other:
+            raise ValueError("Both the 'only_other' and 'only_tamil' arguments can't be true")
+
+        # To get text except Tamil characters
         if only_other:
             non_tamil_text = self.web_content.find_all(string=re.compile('[^ஂ-௺]+'))
             return filter_non_tamil_text(non_tamil_text)
         else:
-            # to get tamil text
+            # To get Tamil text
             tamil_text = self.web_content.find_all(string=re.compile('[ஂ-௺]+'))
 
-            # to get only tamil text
+            # To get only Tamil text
             if only_tamil:
                 return filter_tamil_text(tamil_text)
 
@@ -90,13 +118,27 @@ class TamilScarper:
     def get_by_tag(self, tag: str, only_other: bool = False, only_tamil: bool = False) -> list[list] | list[str]:
         """
         Gives the tamil text(may contain non-tamil characters) (or) only tamil text (or) only non-tamil text,
-        of the tag name mentioned
-        :param tag : The HTML tag name that need to get.
+        of the tag name mentioned.
+
+        ---
+        NOTE
+        ---
+
+        if this function returns empty array it means there is no content available
+
+        (or)
+
+        The Website you are looking is NOT ALLOWING content to be scraped
+
+        :param tag: The HTML tag name that is needed to get the text.
         :param only_other: True, if only non-tamil characters needed.
         :param only_tamil: True, if only tamil characters needed.
-        :return:
+        :return: list[str]: if the function is called normally; list[list]: only_tamil (or) only_other is True.
+        :raise ValueError: If both only_other and only_tamil are True, for one function call one type of output
         """
 
+        if only_tamil and only_other:
+            raise ValueError("Both the 'only_other' and 'only_tamil' arguments can't be true")
         tag_elements = self.web_content.find_all(tag)
         # Extract text content from each element and store in a list
         tag_texts = [element.get_text(strip=True) for element in tag_elements]
@@ -107,19 +149,24 @@ class TamilScarper:
             return filter_non_tamil_text(tag_texts)
         return tag_texts
 
-    def get_table(self) -> list[pandas.DataFrame]:
+    def get_table(self) -> list[pandas.DataFrame] | list:
         """
-        Extracts HTML tables containing Tamil text from a given URL.
+        Gives the table that contains any tamil characters.
+        It will get the table even any of the columns have a single tamil character.
 
-        :return: list: List of pandas DataFrames containing tables with Tamil text.
+        :return: list: List of pandas DataFrames, containing tables with Tamil text.
         """
         # Read HTML tables from the specified URL
-        df_list = pandas.read_html(self.link)
-
+        try:
+            df_list = pandas.read_html(self.link)
+        except HTTPError as e:
+            print('The Website You are trying to scrap is not allowing to get the content, better try another site\n',
+                  e)
+            return []
         # Initialize a list to store DataFrames with Tamil text
         df_tamil_list = []
 
-        # Check each DataFrame for columns containing Tamil text
+        # Iterate to all the tables scraped from the website
         for df in df_list:
             tamil_text_found = False
 
@@ -135,8 +182,3 @@ class TamilScarper:
                 df_tamil_list.append(df)
 
         return df_tamil_list
-
-
-if __name__ == '__main__':
-    extracted_tamil_text = TamilScarper('https://ta.wikipedia.org/wiki/%E0%AE%95%E0%AF%8B%E0%AE%AF%E0%AE%BF%E0%AE%B2%E0%AF%8D_%E0%AE%AE%E0%AE%A3%E0%AE%BF%E0%AE%AF%E0%AF%8B%E0%AE%9A%E0%AF%88')
-    print(extracted_tamil_text.get_table())
